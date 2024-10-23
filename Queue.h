d545daa7432692bbca568269740e0aab4d91646b
komal zahid <pizu622@gmail.com>
2024-10-23 22:48:07 +0500

    Add files via upload

---
+++ Queue.h
@@ -0,0 +1,24 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+#include <iostream>
+using namespace std;
+template <typename T>
+class Queue
+{
+	T * queue;
+	int real;
+	int front;
+	int size;
+public:
+	Queue();
+	Queue(int s);
+	bool isEmpty();
+	bool isFull();
+	void enQueue(T n);
+	T deQueue();
+	T showFront();
+	void display();
+	T DeQueue();
+
+};
+#endif
